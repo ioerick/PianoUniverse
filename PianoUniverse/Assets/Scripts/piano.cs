d00a500ba51997093c42e7dc3cc18dfe864b420a
Erick <ioerick.universe@gmail.com>
2019-08-08 20:25:59 -0400

    PianoDesign

---
+++ PianoUniverse/Assets/Scripts/piano.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class piano : MonoBehaviour
+{
+    public GameObject[] pianoKeys;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pianoKeys = GameObject.FindGameObjectsWithTag("pianoKey");
+        
+        for(int i = 0; i < pianoKeys.Length; i++)
+        {
+            Debug.Log("key number "+i+" is named "+pianoKeys[i].name);
+        }
+    }
+
+    void OnMouseDown ()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PianoUniverse/Assets/Scripts/pianoKey.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pianoKey : MonoBehaviour{
+    public Sprite keyIddle, keyPressed;
+    SpriteRenderer spriteRenderer;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(Input.GetMouseButtonDown(0)) {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos = new Vector2(wp.x, wp.y);
+            if (GetComponent<BoxCollider2D>() == Physics2D.OverlapPoint(mousePos)) {
+                spriteRenderer.sprite = keyPressed;
+                audioSource.Play();
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            spriteRenderer.sprite = keyIddle;
+        }
+    }
+}
